ed64de7c28b5ac698265e2a4cc6f1204945ef079
MazwiMagubane <mazwimagubane@gmail.com>
2025-04-06 15:29:48 +0200

    Initial commit

---
+++ socialproject/users/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['passwords'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("user authenticated and logged in")
+            else:
+                return HttpResponse('Invalid credentials')
+    
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form':form})
+
+@login_required
+def index(request):
+    return render(request, 'users/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, '')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, '', {'user_form':user_form})
